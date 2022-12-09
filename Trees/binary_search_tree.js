20b41ae625ac024eb085da862c01bd1966481ac3
Andreea Pantea <andreea.pantea@fxbits.io>
2022-12-09 13:45:00 +0200

    binary search

--- Trees/binary_search_tree.js
+++ Trees/binary_search_tree.js
@@ -55,12 +55,68 @@ class BinarySearchTree {
         if(!found) return undefined;
         return current;
     }
+    
+    //Breadth first search
+    BFS(){
+        var node = this.root,
+            queue = [],
+            data = [];
+            queue.push(node);
 
+            while(queue.length){
+                node = queue.shift();
+                data.push(node.value);
+                if(node.left) queue.push(node.left);
+                if(node.right) queue.push(node.right);
+            }
+            return data;
+    }
+
+    DFSPreOrder(){
+        var data = [];
+        function traverse(node){
+            data.push(node);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return data;
+    }
+
+    DFSPostOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+        return data;
+
+    }
+
+    DFSInOrder(){
+        var data = [];
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
 
 }
 
 var tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+tree.DFSPreOrder();
+tree.DFSPostOrder();
+tree.DFSInOrder();
